48f6789f9ac2a0cfca8f06541c43c6313a0c563f
general electrix <general.electrix@gmail.com>
2023-09-30 17:16:57 -0700

    Implement animation for dimmer and starlight.

--- src/fixture/dimmer.rs
+++ src/fixture/dimmer.rs
@@ -1,17 +1,19 @@
 //! Control profile for a dimmer.
 
+use num_derive::{FromPrimitive, ToPrimitive};
 use number::UnipolarFloat;
+use strum_macros::{Display as EnumDisplay, EnumIter, EnumString};
 
 use super::{
-    ControllableFixture, EmitFixtureStateChange, FixtureControlMessage,
-    NonAnimatedFixture, PatchFixture,
+    AnimatedFixture, ControllableFixture, EmitFixtureStateChange, FixtureControlMessage,
+    PatchAnimatedFixture,
 };
 use crate::{master::MasterControls, util::unipolar_to_range};
 
 #[derive(Default, Debug)]
 pub struct Dimmer(UnipolarFloat);
 
-impl PatchFixture for Dimmer {
+impl PatchAnimatedFixture for Dimmer {
     const NAME: &'static str = "dimmer";
     fn channel_count(&self) -> usize {
         1
@@ -25,9 +27,21 @@ impl Dimmer {
     }
 }
 
-impl NonAnimatedFixture for Dimmer {
-    fn render(&self, _master_controls: &MasterControls, dmx_buf: &mut [u8]) {
-        dmx_buf[0] = unipolar_to_range(0, 255, self.0);
+impl AnimatedFixture for Dimmer {
+    type Target = AnimationTarget;
+
+    fn render_with_animations(
+        &self,
+        _master: &MasterControls,
+        animation_vals: &super::animation_target::TargetedAnimationValues<Self::Target>,
+        dmx_buf: &mut [u8],
+    ) {
+        let mut level = self.0.val();
+
+        for (val, _target) in animation_vals {
+            level += val;
+        }
+        dmx_buf[0] = unipolar_to_range(0, 255, UnipolarFloat::new(level));
     }
 }
 
@@ -55,3 +69,28 @@ pub type StateChange = UnipolarFloat;
 
 // Venus has no controls that are not represented as state changes.
 pub type ControlMessage = StateChange;
+
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    EnumString,
+    EnumIter,
+    EnumDisplay,
+    FromPrimitive,
+    ToPrimitive,
+)]
+pub enum AnimationTarget {
+    #[default]
+    Level,
+}
+
+impl AnimationTarget {
+    /// Return true if this target is unipolar instead of bipolar.
+    #[allow(unused)]
+    pub fn is_unipolar(&self) -> bool {
+        true
+    }
+}
--- src/fixture/starlight.rs
+++ src/fixture/starlight.rs
@@ -1,15 +1,17 @@
 //! Control profile for the "house light" Starlight white laser moonflower.
 
+use num_derive::{FromPrimitive, ToPrimitive};
 use number::{BipolarFloat, UnipolarFloat};
 
 use super::generic::{GenericStrobe, GenericStrobeStateChange};
 use super::{
-    ControllableFixture, EmitFixtureStateChange as EmitShowStateChange,
-    FixtureControlMessage, NonAnimatedFixture, PatchFixture,
+    AnimatedFixture, ControllableFixture, EmitFixtureStateChange as EmitShowStateChange,
+    FixtureControlMessage, PatchAnimatedFixture,
 };
 use crate::master::MasterControls;
 use crate::util::bipolar_to_split_range;
 use crate::util::unipolar_to_range;
+use strum_macros::{Display as EnumDisplay, EnumIter, EnumString};
 
 #[derive(Default, Debug)]
 pub struct Starlight {
@@ -18,7 +20,7 @@ pub struct Starlight {
     rotation: BipolarFloat,
 }
 
-impl PatchFixture for Starlight {
+impl PatchAnimatedFixture for Starlight {
     const NAME: &'static str = "starlight";
     fn channel_count(&self) -> usize {
         4
@@ -37,14 +39,30 @@ impl Starlight {
     }
 }
 
-impl NonAnimatedFixture for Starlight {
-    fn render(&self, master_controls: &MasterControls, dmx_buf: &mut [u8]) {
+impl AnimatedFixture for Starlight {
+    type Target = AnimationTarget;
+    fn render_with_animations(
+        &self,
+        master_controls: &MasterControls,
+        animation_vals: &super::animation_target::TargetedAnimationValues<Self::Target>,
+        dmx_buf: &mut [u8],
+    ) {
         dmx_buf[0] = 255; // DMX mode
-        dmx_buf[1] = unipolar_to_range(0, 255, self.dimmer);
+        let mut dimmer = self.dimmer.val();
+        let mut rotation = self.rotation.val();
+        for (val, target) in animation_vals {
+            use AnimationTarget::*;
+            match target {
+                // FIXME: might want to do something nicer for unipolar values
+                Rotation => rotation += val,
+                Dimmer => dimmer += val,
+            }
+        }
+        dmx_buf[1] = unipolar_to_range(0, 255, UnipolarFloat::new(dimmer));
         dmx_buf[2] = self
             .strobe
             .render_range_with_master(master_controls.strobe(), 0, 10, 255);
-        dmx_buf[3] = bipolar_to_split_range(self.rotation, 0, 127, 255, 128, 0);
+        dmx_buf[3] = bipolar_to_split_range(BipolarFloat::new(rotation), 0, 127, 255, 128, 0);
     }
 }
 
@@ -83,3 +101,29 @@ pub enum StateChange {
 
 // Starlight has no controls that are not represented as state changes.
 pub type ControlMessage = StateChange;
+
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    EnumString,
+    EnumIter,
+    EnumDisplay,
+    FromPrimitive,
+    ToPrimitive,
+)]
+pub enum AnimationTarget {
+    #[default]
+    Dimmer,
+    Rotation,
+}
+
+impl AnimationTarget {
+    /// Return true if this target is unipolar instead of bipolar.
+    #[allow(unused)]
+    pub fn is_unipolar(&self) -> bool {
+        matches!(self, Self::Dimmer)
+    }
+}
