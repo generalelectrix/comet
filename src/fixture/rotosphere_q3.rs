e7a510572100a178da724882421f424fbe2ee87d
general electrix <general.electrix@gmail.com>
2023-11-05 12:29:31 -0800

    Make rotosphere animatable.

--- src/fixture/rotosphere_q3.rs
+++ src/fixture/rotosphere_q3.rs
@@ -1,14 +1,17 @@
 //! Control profle for the Chauvet Rotosphere Q3, aka Son Of Spherion.
 
+use num_derive::{FromPrimitive, ToPrimitive};
 use number::BipolarFloat;
+use strum_macros::{Display as EnumDisplay, EnumIter, EnumString};
 
+use super::animation_target::TargetedAnimationValues;
 use super::color::{Color, Model as ColorModel, StateChange as ColorStateChange};
 use super::generic::{GenericStrobe, GenericStrobeStateChange};
 use super::{
-    ControllableFixture, EmitFixtureStateChange, FixtureControlMessage, NonAnimatedFixture,
-    PatchFixture,
+    AnimatedFixture, ControllableFixture, EmitFixtureStateChange, FixtureControlMessage,
+    NonAnimatedFixture, PatchAnimatedFixture,
 };
-use crate::master::{Autopilot, MasterControls};
+use crate::master::MasterControls;
 use crate::util::bipolar_to_split_range;
 
 #[derive(Debug)]
@@ -28,7 +31,7 @@ impl Default for RotosphereQ3 {
     }
 }
 
-impl PatchFixture for RotosphereQ3 {
+impl PatchAnimatedFixture for RotosphereQ3 {
     const NAME: &'static str = "rotosphere_q3";
     fn channel_count(&self) -> usize {
         9
@@ -45,42 +48,29 @@ impl RotosphereQ3 {
         };
         emitter.emit_rotosphere_q3(sc);
     }
-
-    fn render_autopilot(&self, autopilot: &Autopilot, dmx_buf: &mut [u8]) {
-        dmx_buf[0] = 0;
-        dmx_buf[1] = 0;
-        dmx_buf[2] = 0;
-        dmx_buf[3] = 0;
-        dmx_buf[4] = 0;
-        dmx_buf[5] = 0;
-        dmx_buf[6] = match autopilot.program() % 5 {
-            0 => 212,
-            1 => 221,
-            2 => 230,
-            3 => 239,
-            4 => 248,
-            _ => 212,
-        };
-        dmx_buf[7] = if autopilot.sound_active() {
-            255
-        } else {
-            50 // TODO is this a good value?  Too slow?
-        };
-        dmx_buf[8] = 30; // TODO is this a good value?  Too slow?
-    }
 }
 
-impl NonAnimatedFixture for RotosphereQ3 {
-    fn render(&self, master: &MasterControls, dmx_buf: &mut [u8]) {
-        if master.autopilot().on() {
-            self.render_autopilot(master.autopilot(), dmx_buf);
-            return;
+impl AnimatedFixture for RotosphereQ3 {
+    type Target = AnimationTarget;
+
+    fn render_with_animations(
+        &self,
+        master: &MasterControls,
+        animation_vals: &TargetedAnimationValues<Self::Target>,
+        dmx_buf: &mut [u8],
+    ) {
+        let mut rotation = self.rotation.val();
+        for (val, target) in animation_vals {
+            use AnimationTarget::*;
+            match target {
+                Rotation => rotation += val,
+            }
         }
         self.color.render(master, &mut dmx_buf[0..4]);
         dmx_buf[4] = self
             .strobe
             .render_range_with_master(master.strobe(), 0, 1, 250);
-        dmx_buf[5] = bipolar_to_split_range(self.rotation, 1, 127, 129, 255, 0);
+        dmx_buf[5] = bipolar_to_split_range(BipolarFloat::new(rotation), 1, 127, 129, 255, 0);
         dmx_buf[6] = 0;
         dmx_buf[7] = 0;
         dmx_buf[8] = 0;
@@ -124,3 +114,28 @@ pub enum StateChange {
 }
 
 pub type ControlMessage = StateChange;
+
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    EnumString,
+    EnumIter,
+    EnumDisplay,
+    FromPrimitive,
+    ToPrimitive,
+)]
+pub enum AnimationTarget {
+    #[default]
+    Rotation,
+}
+
+impl AnimationTarget {
+    /// Return true if this target is unipolar instead of bipolar.
+    #[allow(unused)]
+    pub fn is_unipolar(&self) -> bool {
+        false
+    }
+}
