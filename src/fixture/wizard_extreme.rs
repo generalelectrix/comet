7638bdae9f6c93b62aaff80c7946ef5a08124d07
general electrix <general.electrix@gmail.com>
2024-05-30 21:50:47 -0700

    Add mirroring control to wizard extreme.

--- src/fixture/wizard_extreme.rs
+++ src/fixture/wizard_extreme.rs
@@ -129,8 +129,14 @@ impl AnimatedFixture for WizardExtreme {
                 strobe
             }
         };
-        dmx_buf[1] =
-            bipolar_to_split_range(BipolarFloat::new(reflector_rotation), 2, 63, 127, 66, 0);
+        dmx_buf[1] = bipolar_to_split_range(
+            BipolarFloat::new(reflector_rotation).invert_if(group_controls.mirror),
+            2,
+            63,
+            127,
+            66,
+            0,
+        );
 
         dmx_buf[2] = if self.twinkle {
             // WHY did you put twinkle on the color wheel...
@@ -141,8 +147,19 @@ impl AnimatedFixture for WizardExtreme {
         dmx_buf[3] = 0; // color shake
         dmx_buf[4] = (self.gobo as u8) * 12;
         dmx_buf[5] = 0; // gobo shake
-        dmx_buf[6] = bipolar_to_range(0, 127, BipolarFloat::new(drum_swivel));
-        dmx_buf[7] = bipolar_to_split_range(BipolarFloat::new(drum_rotation), 2, 63, 127, 66, 0);
+        dmx_buf[6] = bipolar_to_range(
+            0,
+            127,
+            BipolarFloat::new(drum_swivel).invert_if(group_controls.mirror),
+        );
+        dmx_buf[7] = bipolar_to_split_range(
+            BipolarFloat::new(drum_rotation).invert_if(group_controls.mirror),
+            2,
+            63,
+            127,
+            66,
+            0,
+        );
         dmx_buf[8] = 0;
         dmx_buf[9] = 0;
         dmx_buf[10] = 0;
