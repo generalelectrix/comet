faa6371dda3025a925299f76bcbd4d68bb2f61f7
general electrix <general.electrix@gmail.com>
2024-11-15 02:29:45 -0800

    Poll WLED until successful connection is made.

--- src/wled.rs
+++ src/wled.rs
@@ -1,7 +1,10 @@
 //! Use a thread to perform asynchronous communication with a WLED instance.
 use anyhow::Result;
-use log::{error, info};
-use std::sync::mpsc::{channel, Sender};
+use log::{error, info, warn};
+use std::{
+    sync::mpsc::{channel, Sender},
+    time::Duration,
+};
 
 use anyhow::Context;
 use reqwest::Url;
@@ -15,11 +18,14 @@ pub struct WledController {
 
 impl WledController {
     /// Run a thread to handle sending WLED control messages.
+    ///
+    /// Polls once every 5 seconds for initial configuration until the client
+    /// responds.
     pub fn run(addr: &str, _send: Sender<ControlMessage>) -> Result<Self> {
-        let mut wled = Wled::try_from_url(&Url::try_from(addr).context("parsing WLED URL")?)
-            .context("creating WLED instance")?;
+        let url = Url::try_from(addr).context("parsing WLED URL")?;
         let (send_state, recv_state) = channel();
         std::thread::spawn(move || {
+            let mut wled = initialize(&url, Duration::from_secs(5));
             for msg in recv_state {
                 match msg {
                     WledControlMessage::SetState(state) => {
@@ -40,6 +46,23 @@ impl WledController {
             info!("WLED handler thread shutting down.");
         });
         Ok(Self { send: send_state })
+    }
+}
+
+/// Poll the WLED API until we get a good config back.
+fn initialize(url: &Url, poll_interval: Duration) -> Wled {
+    info!("Initializing WLED...");
+    loop {
+        match Wled::try_from_url(url) {
+            Ok(wled) => {
+                info!("WLED client connection successful.");
+                return wled;
+            }
+            Err(err) => {
+                warn!("Failed to initialize WLED at {url}: {err}");
+            }
+        }
+        std::thread::sleep(poll_interval);
     }
 }
 
